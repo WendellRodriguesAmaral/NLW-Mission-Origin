0e15de900c31f6b9d854cd6f4278b19620e876dd
Wendell Rodrigues Amaral <wendellrodrigues97@hotmail.com>
2021-08-18 22:19:58 -0300

    finish footer mobile version

--- main.js
+++ main.js
@@ -1,67 +1,74 @@
 /* abre e fecha o menu */
-const nav =  document.querySelector("#header nav")
-const toggle = document.querySelectorAll("nav .toogle") //Retorna NodeList
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toogle"); //Retorna NodeList
 
-for(const element of toggle){
-    element.addEventListener("click", ()=>{
-        nav.classList.toggle("show")
-    })
+for (const element of toggle) {
+  element.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
 }
 
- /* quando clicar em algum item de menu, esconder o menu */
+/* quando clicar em algum item de menu, esconder o menu */
 
- const links = document.querySelectorAll('nav ul  li a')
+const links = document.querySelectorAll("nav ul  li a");
 
- for(link of links){
-     link.addEventListener('click', ()=>{
-         nav.classList.remove("show")
-     })
- }
-
-
-
- /* mudar o menu da pagina quando der scroll */
-
- const header = document.querySelector("#header")  
- const navHeight = header.offsetHeight
-
-
-  window.addEventListener('scroll', ()=>{
-    if(window.scrollY >= navHeight){ //se scroll maior ou igual a altura da barra de menu
-        header.classList.add("scroll")
-    }else{
-        header.classList.remove("scroll")
-    }
-
-  })
-
-
-  /* Testimonials cards */
-
-  const swiper = new Swiper('.swiper-container', {
-   slidesPerview:1,
-   pagination:{
-       el:".swiper-pagination"
-   },
-   mousewheel:true,
-   keyboard:true
-
+for (link of links) {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
   });
+}
 
+/* Testimonials cards */
 
-  /*  scroll reveal: mostrar os elementos quando dar o scroll na pag   */
+const swiper = new Swiper(".swiper-container", {
+  slidesPerview: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 
-  const scrollReveal = ScrollReveal({
-    origin:'top',
-    distance:'30px',
-    duration:700,
-    reset:true
-  })
+/*  scroll reveal: mostrar os elementos quando dar o scroll na pag   */
 
-  scrollReveal.reveal(
-      `#home .image,  #home .text,
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `#home .image,  #home .text,
       #about .image,  #about .text,
       #services header,  #services .card,
       #testimonials header,  #home .testimonials, 
-      #contact .text, #contact .links`,
-      {interval:100})
+      #contact .text, #contact .links,
+      footer .brand, footer .social  
+      `,
+  { interval: 100 }
+);
+
+/* mudar o menu da pagina quando der scroll */
+
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= navHeight) {//se scroll maior ou igual a altura da barra de menu
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
+
+/* botão voltar para o topo  */
+
+const backToTopButton = document.querySelector(".back-to-top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+});
